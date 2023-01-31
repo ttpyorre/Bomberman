d8e405fb5d00524a56060c568ad004a7a8463aa0
ttpyorre <ttpyorre@wpi.edu>
2023-01-31 00:34:27 -0500

    renamed bomberman to lowercase for pathing work, astar created

---
+++ team02/astarCharacter.py
@@ -0,0 +1,85 @@
+# This is necessary to find the main code
+import sys
+sys.path.insert(0, '../bomberman')
+# Import necessary stuff
+from entity import CharacterEntity
+from colorama import Fore, Back
+
+# Import Priority Queue
+from priorityQueue import PriorityQueue
+import numpy as np
+import math
+
+class AstarCharacter(CharacterEntity):
+
+    def do(self, wrld):
+        # Astar gives us a path of nodes that we visit one by one
+        path = self.astar(wrld)
+
+        while path:
+            walk = path.pop()
+            self.move(walk[0] - self.x, walk[1] - self.y)
+
+
+
+    @staticmethod
+    def neighbors_of_8(wrld, x, y):
+        '''
+        Returns walkable neighbor cells of the cell we are currently in.
+        :param wrld     [SensedWorld]   world object
+        :param x        [int]           x coordinate in world
+        :param y        [int]           y coordinate in world
+        :return         [[(int, int)]]  list of all neighbors that are available
+        '''
+        # we search with right, down, left, up priority
+        neighbors = [(x + 1, y), (x + 1, y + 1), (x, y + 1), (x - 1, y + 1), (x - 1, y), (x - 1, y - 1), (x, y - 1), (x + 1, y - 1)]
+        availableNeighbors = []
+
+        for neighbor in neighbors:
+            if neighbor[0] >= 0 and neighbor[1] >= 0 and neighbor[0] < wrld.width() and neighbor[1] < wrld.height():
+                if wrld.wall_at(neighbor[0], neighbor[1]) != True:
+                    availableNeighbors.append(neighbor)
+
+        return availableNeighbors
+                
+
+    @staticmethod
+    def heuristics(start, goal):
+        return math.sqrt(pow(abs(start[0] - goal[0]), 2) + pow(abs(start[1] - goal[1]), 2))
+
+    def astar(self, wrld):
+        start = (self.x, self.y)
+        goal = wrld.exitcell
+
+        # Establishing our queue
+        queue = PriorityQueue()
+        queue.put(start, 0)
+        
+        cameFrom = {}
+        cost = {}
+        cameFrom[start] = None
+        cost[start] = 0
+
+        exploredMap = []
+        path = []
+
+        while not queue.empty():
+            current = queue.get()
+
+            if goal == current:
+                break
+
+            nextNodes = AstarCharacter.neighbors_of_8(wrld, current[0], current[1])
+            for nextNode in nextNodes:
+                new_cost = cost[current]
+                if nextNode not in cost or new_cost < cost[nextNode]:
+                    cost[nextNode] = new_cost
+                    priority = new_cost + AstarCharacter.heuristics(nextNode, goal)
+                    queue.put(nextNode, priority)
+                    cameFrom[nextNode] = current
+
+        while current != start:
+            path.insert(0, current)
+            current = cameFrom[current]
+
+        return path
