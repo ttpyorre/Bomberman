52d54fc6abc92a3e3c0b3b81878c0cca9af8b46e
Tuomas Pyorre <82783219+ttpyorre@users.noreply.github.com>
2023-02-08 23:40:16 -0500

    Revert "Variant2"

--- teamNN/project2/variant2.py
+++ teamNN/project2/variant2.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env Python3
 # This is necessary to find the main code
 import sys
 sys.path.insert(0, '../../bomberman')
@@ -7,19 +6,18 @@ sys.path.insert(1, '..')
 # Import necessary stuff
 import random
 from game import Game
-from monsters.selfpreserving_monster import SelfPreservingMonster
+from monsters.stupid_monster import StupidMonster
 
 # TODO This is your code!
-sys.path.insert(1, '../team02')
+sys.path.insert(1, '../teamNN')
 from testcharacter import TestCharacter
 
 # Create the game
 random.seed(123) # TODO Change this if you want different random choices
 g = Game.fromfile('map.txt')
-g.add_monster(SelfPreservingMonster("aggressive", # name
-                                    "A",          # avatar
-                                    3, 13,        # position
-                                    2             # detection range
+g.add_monster(StupidMonster("stupid", # name
+                            "S",      # avatar
+                            3, 9      # position
 ))
 
 # TODO Add your character
