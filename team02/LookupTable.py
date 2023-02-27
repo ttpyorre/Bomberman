36de1ea940fe117c0195bf61f1f4c53a24ff1654
Hoang Nguyen <hcnguyen@wpi.edu>
2023-02-27 05:29:15 -0500

    found a bug with NLookuptable, fixed it

--- team02/LookupTable.py
+++ team02/LookupTable.py
@@ -4,9 +4,14 @@ sys.path.insert(0, '../bomberman')
 
 class StateActionLookupTable():
 
+    states = []
+    actions = []
     table = {}
 
     def __init__(self, states = [], actions = [] ):
+
+        self.states = states
+        self.actions = actions
 
         # create an empty dictionary that can be indexed by state and action
         for s in states:
@@ -22,13 +27,38 @@ class StateActionLookupTable():
     def update(self, s, a, value):
         self.table[(s,a)] += value
 
-class NStateActionTable(StateActionLookupTable):
+    def printit(self):
+
+        print(self.table)
+                
+
+
+
+class NStateActionTable():
     """
     This table holds the frequency (number of times) we explored an state-action pair
     """
 
+    states = []
+    actions = []
+    table = {}
+
+    def __init__(self, states = [], actions = [] ):
+
+        self.states = states
+        self.actions = actions
+
+        # create an empty dictionary that can be indexed by state and action
+        for s in states:
+            for a in actions:
+                self.table[(s, a)] = 0
+
+    def get(self, s , a):
+        return self.table[(s,a)]
+
     def update(self, s, a):
-        """
-        Add 1 to the state-action we just did, and update the table accordingly
-        """
-        self.table[(s, a)] += 1.0
+        self.table[(s,a)] += 1
+
+    def printit(self):
+        print(self.table)
+                
